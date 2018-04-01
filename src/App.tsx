121fb930a6d1a712bb85f333a2072bfdf79bb1f0
Yankuan Zhang <yankuan_zhang@esri.com>
2018-04-01 11:38:05 -0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      This react should be hosted on github pages.
+    </div>
+  );
+};
+
+export default App;
